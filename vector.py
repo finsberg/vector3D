20d66903c27c4cfcfe344a45e8109451463f1189
Henrik Finsberg <henriknf@simula.no>
2019-09-01 19:41:42 +0200

    Add some docstrings

--- vector.py
+++ vector.py
@@ -1,7 +1,39 @@
+"""
+This module contains the Vector3D class to be used 
+for illustrating how to document and test a project.
+
+A good example of the NumPy style can be found here:
+https://sphinxcontrib-napoleon.readthedocs.io/en/latest/example_numpy.html
+
+"""
 import numpy as np
 
 
 class Vector3D:
+    """
+    A class for creating 3D vectors
+
+    Arguments
+    ---------
+    x : float
+        x - coordinate
+    y : float
+        y - coordinate
+    z : float
+        z - coordinate
+
+    Example
+    -------
+    .. code:: 
+
+        >>> u = Vector3D(1, 1, 1)
+        >>> v = Vector3D(1, 2, 3)
+        >>> w = u + v
+        >>> print(w)
+        (2, 3, 4)
+
+    """
+
     def __init__(self, x, y, z):
         self.x = x
         self.y = y
@@ -43,9 +75,50 @@ class Vector3D:
         return Vector3D(x, y, z)
 
     def dot(self, other):
+        r"""Compute the dot product
+
+        .. math::
+
+            u \cdot v = u_1v_1 + u_2v_2 + u_3v_3
+
+        Arguments
+        ---------
+        other : Vector3D
+            Vector that you want to dot
+
+        Returns
+        -------
+        float
+            The dot product
+        """
         return self.x * other.x + self.y * other.y + self.z * other.z
 
     def cross(self, other):
+        r"""Compute the cross product
+
+        .. math::
+
+            w = u \times v
+
+        with
+
+        .. math::
+
+            w_1 &= u_2 v_3 -  u_3 v_2 \\
+            w_2 &= u_1 v_3 -  u_3 v_1 \\
+            w_3 &= u_1 v_2 -  u_2 v_1
+
+
+        Arguments
+        ---------
+        other : Vector3D
+            Vector that you want to dot
+
+        Returns
+        -------
+        float
+            The dot product
+        """
         x = self.y * other.z - self.z * other.y
         y = self.z * other.x - self.x * other.z
         z = self.x * other.y - self.y * other.x
@@ -73,16 +146,30 @@ class Vector3D:
 
     @property
     def length(self):
+        """Get length of vector (2-norm)
+        """
         return np.sqrt(self * self)
 
     @length.setter
     def length(self, new_length):
+        """Set length of the vector
+
+        Raises
+        ------
+        ValueError
+            If you try to set length equal a non-positive value
+        """
+        if new_length < 1e-12:
+            raise ValueError("Cannot set length equal to zero or a negative value")
         scale = new_length / self.length
         self.x *= scale
         self.y *= scale
         self.z *= scale
 
     def unit(self):
+        """"Return a vector in pointing in the same direction
+        of unit length
+        """"
         new_vector = Vector3D(self.x, self.y, self.z)
         new_vector.length = 1
         return new_vector
